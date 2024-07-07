533d410bb22e4354684c57b6cc3a95a40919dc54
surajbartoula <suraj_bartoula@yahoo.com>
2024-07-07 23:24:48 +0400

    changes in main.c

--- minishell/main/main.c
+++ minishell/main/main.c
@@ -6,64 +6,18 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:31:48 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/07/03 15:32:59 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/07/07 23:24:30 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "../include/minishell.h"
 
-static char to_lower(char c)
+int	main(int argc, char *argv[], char *envp[])
 {
-    if (c >= 'A' && c <= 'Z')
-            return (c + 32);
-    return c;
-}
-
-static int get_digit(char c, int base)
-{
-    int max;
-    if (base <= 10)
-        max = c + '0';
-    else 
-        max = base - 10 + 'a';
-    if (c >= '0' && c <= '9' && c <= max)
-        return (c - '0');
-    else if (c >= 'a' && c <= 'f' && c <= max)
-        return (c + 10 - 'a');
-    else
-        return -1;
-}
-
-int ft_atoi_base(char *s, int base)
-{
-    int sign = 1;
-    int res = 0;
-    int digit;
-
-    if (*s == '-')
-    {
-        sign = -1;
-        s++;
-    }
-
-    while((digit = get_digit(to_lower(*s), base)) > 0)
-    {
-        res = (res * base);
-        res = (res * sign) + digit;
-        s++;
-    }
-    return res;
-}
-
-int main(int ac, char **av)
-{
-    if (ac == 3)
-    {
-        char *num_str = av[1];
-        int base = atoi(av[2]);
-        int num = ft_atoi_base(num_str, base);
-        printf("Converted number: %d\n", num);
-    }
-    else
-        printf("Wrong Number of Args!\n");
-    return 0;
+	if (argc != 1)
+	{
+		printf("This program does not accept arguments\n");
+		return (0);
+	}
+	return (0);
 }
