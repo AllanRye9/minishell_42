dde2ed22936b3311c2e430bf5538277ecb062c9d
surajbartoula <suraj_bartoula@yahoo.com>
2024-07-19 18:05:24 +0400

    working on command cd

--- minishell/built-ins/cd.c
+++ minishell/built-ins/cd.c
@@ -1,14 +1,46 @@
 #include "../include/minishell.h"
+
+char	*find_home(t_sysvar *sysvar, char *path)
+{
+	int	i;
+
+	while (sysvar->env[i])
+	{
+		if (!ft_strncmp(sysvar->env[i], path, ft_strlen(path)))
+			return (ft_substr(sysvar->env[i], ft_strlen(path), ft_strlen(sysvar->env[i]) - ft_strlen(path)));
+		i++;
+	}
+	return (NULL);
+}
  
-void	change_home_dir(t_sysvar *sys_var, char *str)
+int	change_dir(t_sysvar *sys_var, char *path)
 {
 	char	*dir;
+	int		ret_value;
 
-	dir = get_dir(str, sys_var);
+	dir = find_home(sys_var, path);
 }
 
-int	change_dir(t_sysvar *sys_var, t_cmds *cmds)
+int	change_dir(t_sysvar *sysvar, char *path)
 {
-	if (!cmds->str[1])
-		change_home_dir(sys_var, "HOME=");
+	int	ret_val;
+
+	if (!path)
+		ret_val = change_dir(sysvar, "HOME=");
+	else if (ft_strncmp(path, "-", 1) == 0)
+		ret_val = change_dir(sysvar, "OLDPWD=");
+	else
+	{
+		ret_val = chdir(path);
+		if (ret_val == -1)
+		{
+			ft_putstr_fd("Error:", STDERR_FILENO);
+			ft_putstr_fd(path, STDERR_FILENO);
+		}
+	}
+	if (ret_val == -1)
+		return (EXIT_FAILURE);
+	change_path(sysvar);
+	add_path_to_env(sysvar);
+	return (EXIT_SUCCESS);
 }
