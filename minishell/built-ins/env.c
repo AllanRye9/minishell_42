ed62b33c0ba26cfa58f317345f73f3e459060468
surajbartoula <suraj_bartoula@yahoo.com>
2024-07-20 23:29:47 +0400

    env done

--- minishell/built-ins/env.c
+++ minishell/built-ins/env.c
@@ -6,13 +6,20 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 17:05:21 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/07/18 17:05:58 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/07/20 23:29:21 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	env()
+int	env(t_sysvar *sys_var)
 {
-	
+	int	i;
+
+	i = -1;
+	while (sys_var->env[++i])
+	{
+		ft_putendl_fd(sys_var->env[i], STDOUT_FILENO);
+	}
+	return (EXIT_SUCCESS);
 }
