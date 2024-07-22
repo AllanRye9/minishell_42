d4419072ab503ec551877d3dec3be3ceb3c29f7c
surajbartoula <suraj_bartoula@yahoo.com>
2024-07-22 13:30:24 +0400

    adding export

--- minishell/built-ins/export.c
+++ minishell/built-ins/export.c
@@ -1,2 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 17:42:55 by sbartoul          #+#    #+#             */
+/*   Updated: 2024/07/22 13:30:10 by sbartoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/minishell.h"
 
+int	custom_export(t_sysvar *sys_var, char **argv)
+{
+	char	**tmp;
+	int		i;
+
+	i = -1;
+	if (argv[1] || argv[1][0] == '\0')
+		env(sys_var);
+	else
+	{
+		while (argv[i])
+		{
+			if (valid_param(argv[i]) && variable_exist(sys_var, argv[i]))
+			{
+				if (argv[i])
+				{
+					tmp = add_var(sys_var->env, argv[i]);
+					free_doublearr(sys_var->env);
+					sys_var->env = tmp;
+				}
+			}
+			i++;
+		}
+	}
+	return (EXIT_SUCCESS);
+}
