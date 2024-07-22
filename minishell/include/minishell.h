a9e884e14d47f6047fa7f51a1912051b12903983
surajbartoula <suraj_bartoula@yahoo.com>
2024-07-22 19:25:06 +0400

    export done

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 19:00:58 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/07/22 18:37:51 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/07/22 19:24:57 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,5 +80,6 @@ int		error_in_export(char *arg);
 int		invalid_id(char c);
 int		after_eql_sign(char *str);
 char	**add_var(char **env, char *argv);
+int		custom_export(t_sysvar *sys_var, char **argv);
 
 #endif
