191e4ef60df7cbcc6490837310e60ee0cdc20c6f
surajbartoula <suraj_bartoula@yahoo.com>
2024-07-23 12:06:31 +0400

    working on unset

--- minishell/built-ins/unset.c
+++ minishell/built-ins/unset.c
@@ -6,55 +6,91 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 21:28:39 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/07/22 22:10:12 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/07/23 12:05:59 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	check_key(char *args)
+char	**del_loop(char **env, char **ret_strings, char *argv)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (env[i] != NULL)
+	{
+		if (!(ft_strncmp(env[i], argv, after_eql_sign(env[i]) - 1) == 0
+				&& argv[after_eql_sign(env[i])] == '\0'
+				&& env[i][ft_strlen(argv)] == '='))
+		{
+			ret_strings = ft_strdup(env[i]);
+			if (ret_strings[j] == NULL)
+			{
+				free_doublearr(ret_strings);
+				return (ret_strings);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (ret_strings);
+}
+
+char	**del_var(char **env, char *argv)
+{
+	char	**ret_strings;
+	int		i;
+
+	i = 0;
+	while (env[i] != NULL)
+		i++;
+	ret_strings = ft_calloc(sizeof(char *), i + 1);
+	if (!ret_strings)
+		return (NULL);
+	ret_strings = del_loop(env, ret_strings, argv);
+}
+
+int	error_in_unset(char **args)
 {
 	int	i;
 
-	i = -1;
-	if (!ft_isalpha(*args) && *args != '_')
-		return (0);
-	while (args[i] && args[i] != '=')
+	i = 0;
+	if (!args[1])
 	{
-		if (!ft_isalnum(args[i]) && args[i] != '_')
-			return (0);
+		ft_putendl_fd("minishell: unset: not enough arguments", STDERR_FILENO);
+		return (EXIT_FAILURE);
 	}
-	return (1);
+	while (args[1][i])
+	{
+		if (args[1][i] == '/')
+		{
+			ft_putstr_fd("minishell: unset: `", STDERR_FILENO);
+			ft_putstr_fd(args[1], STDERR_FILENO);
+			ft_putendl_fd("': not a valid identifier", STDERR_FILENO);
+			return (EXIT_FAILURE);
+		}
+	}
+	if (after_eql_sign(args[1] != 0))
+	{
+		ft_putendl_fd("minishell: unset: not a valid identifier", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
 
 int	unset(t_sysvar *sys_var, char **args)
 {
-	int	i;
-	int	ret_val;
+	char	**tmp;
 
-	i = 1;
-	if (!args[1])
-		return (0);
-	ret_val = 0;
-	while (args[i])
+	if (error_in_unset(args) == 1)
+		return (EXIT_FAILURE);
+	else
 	{
-		if (!check_key(args[i]))
-		{
-			ft_putstr_fd("minishell: unset: ", STDERR_FILENO);
-			ft_putstr_fd(args[i], STDERR_FILENO);
-			ft_putendl_fd(": not a valid identifier", STDERR_FILENO);
-			ret_val = 1;
-		}
-		else
-		{
-			if (unsetenv(args[i]) != 0)
-			{
-				ft_putstr_fd("minishell: unset error clearing variables", STDERR_FILENO);
-				ft_putendl_fd(args[i], 2);
-				ret_val = 1;
-			}
-		}
-		i++;
+		tmp = del_var(sys_var->env, args[1]);
+		free_doublearr(sys_var->env);
+		sys_var->env = tmp;
 	}
-	return (ret_val);
+	return (EXIT_SUCCESS);
 }
