28f9e277e45c2ba866b2dc6e7bfe7bff90032c83
ory9 <163534977+ory9@users.noreply.github.com>
2024-07-24 08:28:00 +0000

    files added

--- minishell/signals/signals.c
+++ minishell/signals/signals.c
@@ -1,11 +1,38 @@
-#include "../include/minishell.h"
+#include "minishell.h"
 
-void	sig_handler(int code)
+static void	sig_handler(int code)
 {
-	if (code == 2)
+	(void)code;
+	if(G_shell_.signint_child)
 	{
-		printf("\nminishell>>> \b");;
+		ft_putstr_fd("\n", 1);
+		G_shell_.signint_child = false;
+		G_shell_.heredoc_sigint = true;
 	}
-	else if (code == 3)
-		return ;
+	else
+	{
+		ft_putchar_fd("\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
+void ft_sigquit_handler(int code)
+{
+	(void)code;
+	ft_putchar_fd("Quit: 3\n", 1);
+}
+
+void signal_init_(void)
+{
+	struct termios t;
+
+	t = G_shell_.orignal_terminal;
+	t.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &t);
+	G_shell_.heredoc_sigint = false;
+	G_shell_.signint_child = false;
+	signal(SIGINT, sig_handler);
+	signal(SIGQUIT, SIG_IGN);
 }
