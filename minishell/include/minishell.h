d12e58829584d541ca72ee877208a0f22d9293cc
ory9 <163534977+ory9@users.noreply.github.com>
2024-07-28 10:24:59 +0400

    Update minishell.h

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -33,17 +33,17 @@
 # include "parsing.h"
 # include "tokenizing.h"
 
-int	sig_pipe[2];
+int		sig_pipe[2];
 
 typedef struct s_minishell
 {
 	char				*line;
 	t_token				*tokens;
 	t_token				*curr_token;
-	int					exit_s;
-	t_parse_error		parse_error;
-	int					stdin;
-	int					stdout;
+	int				exit_s;
+	t_parse_error			parse_error;
+	int				stdin;
+	int				stdout;
 	char				**environ;
 	t_node				*ast;
 	t_env				*envlst;
@@ -53,25 +53,25 @@ typedef struct	s_signal_handler
 {
 	bool			heredoc_sgint;
 	bool			signint_child;
-	struct 	termios	original_term;
+	struct 	termios		original_term;
 	bool			cleanup_needed;
 }	t_signal_handler;
 
 t_signal_handler g_sig_handler;
 
 //Builtins functions
-void	change_path(t_sysvar *sys_var);
+void		change_path(t_sysvar *sys_var);
 int		change_dir(t_sysvar *sysvar, char *path);
-void	add_path_to_env(t_sysvar *sys_var);
+void		add_path_to_env(t_sysvar *sys_var);
 int		env(t_sysvar *sys_var);
 int		is_digit(char *str);
-void	free_sysvar(t_sysvar *sys_var);
-void	free_doublearr(char **str);
+void		free_sysvar(t_sysvar *sys_var);
+void		free_doublearr(char **str);
 int		custom_exit(t_sysvar *sys_var, char **args);
 int		error_in_export(char *arg);
 int		invalid_id(char c);
 int		after_eql_sign(char *str);
-char	**add_var(char **env, char *argv);
+char		**add_var(char **env, char *argv);
 int		custom_export(t_sysvar *sys_var, char **argv);
 int		unset(t_sysvar *sys_var, char **args);
 
