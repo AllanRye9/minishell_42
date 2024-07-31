d969e9115d8d72157aa4ab1367d9d2f5c5625138
surajbartoula <suraj_bartoula@yahoo.com>
2024-07-31 11:04:35 +0000

    changing int main

--- minishell/start/start.c
+++ minishell/start/start.c
@@ -6,64 +6,65 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:31:48 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/07/24 10:04:58 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/07/31 11:04:19 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static void	init_environment(char **env, t_sysvar *sys_var)
+static void	init_environment(char **env, t_minishell *g_shell)
 {
-	ft_memset(sys_var, 0, sizeof(t_sysvar));
-	sys_var->env = env;
-	ft_init_env();
-	sys_var->stdin = dup(0);
-	sys_var->stdout = dup(1);
-	get_attribute(STDIN_FILENO, sys_var.original_term);  //need to work here no original term declared yet.
+	ft_memset(g_shell, 0, sizeof(t_minishell));
+	g_shell->environ = env;
+	ft_init_envlst(g_shell);
+	g_shell->stdin = dup(0);
+	g_shell->stdout = dup(1);
+	tcgetattr(STDIN_FILENO, g_shell.original_term);
 }
 
-static void	ft_exec(t_sysvar *sys_var)
+static void	ft_exec(t_minishell *g_shell)
 {
 	signal(SIGQUIT, sigquit_handler);
-	ft_init_tree(sys_var->node);
-	if (sys_var->heredoc_sigint)
+	ft_init_tree(g_shell->ast, g_shell);
+	if (g_sig_handler.heredoc_sigint)
 	{
-		ft_clear_node(sys_var->node);
-		sys_var->heredoc_sigint = false;
+		ft_clear_node(g_shell->ast);
+		g_shell->heredoc_sigint = false;
 	}
-	set_attribute(STDIN_FILENO, 2, sys_var.orignal_term);
-	sys_var->exit_status = ft_exec_node(sys_var->node, false);
-	ft_clear_node(sys_var->node);
+	set_attribute(STDIN_FILENO, 2, g_shell.orignal_term);
+	g_shell->exit_status = ft_exec_node(g_shell->ast, false);
+	ft_clear_node(g_shell->ast);
 }
 
 
-int main(int argc, char **argv, char **env) {
-    t_sysvar sys_var;
+int main(int argc, char **argv, char **env)
+{
+    t_minishell g_shell;
 
     (void)argc;
     (void)argv;
-    init_environment(env, &sys_var);
+    init_environment(env, &g_shell);
     while (1)
 	{
         init_signals();
-        sys_var.args = readline(PROMPT_MSG);
-        if (!sys_var.args)
+        g_shell.args = readline(PROMPT_MSG);
+        if (!g_shell.args)
 		{
             ft_clean();
             ft_putstr_fd("exit\n", STDOUT_FILENO);
-            exit(sys_var.exit_status);
+            exit(g_shell.exit_status);
         }
-        if (sys_var.args[0])
-            add_history(sys_var.args);
-        sys_var.tokens = ft_token();
-        if (sys_var.tokens)
+        if (g_shell.args[0])
+            add_history(g_shell.args);
+        g_shell.tokens = ft_token();
+        if (g_shell.tokens)
 		{
-            sys_var.node = ft_parsing();
-            if (!sys_var.parsing_error.error_type)
-                ft_exec(&sys_var);
+            g_shell.ast = ft_parsing();
+            if (!g_shell.parsing_error.error_type)
+                ft_exec(&g_shell);
             else
                 ft_handle_error();
         }
     }
-    return (ft_clean(), sys_var.exit_status);
+    return (ft_clean(), g_shell.exit_status);
 }
