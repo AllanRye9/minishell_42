e1b69e2edec833662a5af882b4d4c85bf2948d72
surajbartoula <suraj_bartoula@yahoo.com>
2024-08-01 17:22:39 +0400

    init tree added

---
+++ minishell/execution/here_doc.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_doc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 15:39:23 by sbartoul          #+#    #+#             */
+/*   Updated: 2024/08/01 15:47:58 by sbartoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	heredoc_sigint(int sig)
+{
+	(void)sig;
+	g_sig_handler.cleanup_needed = true;
+	exit(SIGINT);
+}
+
+void	ft_heredoc(t_io_node *io, int fd[2], t_minishell *g_shell)
+{
+	char	*line;
+	char	*quote;
+
+	signal(SIGINT, heredoc_sigint);
+	if (g_sig_handler.cleanup_needed)
+		ft_clean_ms(g_shell);
+	quote = io->value;
+	while (*quote && *quote != '"' && *quote != '\'')
+		quote++;
+	while (1)
+	{
+		line = readline("> ");
+		if (!line || ft_is_delim(io->value, line))
+			break ;
+		if (!quote)
+			ft_heredoc_expander(line, fd[1], g_shell);
+		else
+			ft_putendl_fd(line, fd[1]);
+		free(line);
+	}
+	exit(0);
+}
--- minishell/execution/init_tree.c
+++ minishell/execution/init_tree.c
@@ -6,14 +6,89 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 02:53:00 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/08/01 15:24:42 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/08/01 16:52:22 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+static bool	ft_exit_leaf(int fd[2], int *pid)
+{
+	waitpid(*pid, pid, 0);
+	signal(SIGQUIT, sigquit_handler);
+	g_sig_handler.signint_child = false;
+	close(fd[1]);
+	if (WIFEXITED(*pid) && (WEXITSTATUS(*pid) == SIGINT))
+		return (true);
+	return (false);
+}
+
+static void	init_leaf(t_node *node, t_minishell *g_shell)
+{
+	t_io_node	*io;
+	int			fd[2];
+	int			pid;
+
+	if (node->args)
+		node->expanded_args = ft_expand(node->args, g_shell);
+	io = node->io_list;
+	while (io)
+	{
+		if (io->type == IO_HEREDOC)
+		{
+			pipe(fd);
+			g_sig_handler.signint_child = true;
+			pid = (signal(SIGQUIT, SIG_IGN), fork());
+			if (!pid)
+				ft_heredoc(io, fd, g_shell);
+			if (ft_exit_leaf(fd, &pid))
+				return;
+			io->here_doc = fd[0];
+		}
+		else
+			io->expanded_value = ft_expand(io->value, g_shell);
+		io = io->next;
+	}
+}
+
+static void	init_tree(t_node *node, t_minishell *g_shell)
+{
+	t_io_node	*io;
+	int			fd[2];
+	int			pid;
+
+	if (node->args)
+		node->expanded_args = ft_expand(node->args, g_shell);
+	io = node->io_list;
+	while (io)
+	{
+		if (io->type == IO_HEREDOC)
+		{
+			pipe(fd);
+			g_sig_handler.signint_child = false;
+			pid = (signal(SIGQUIT, SIG_IGN), fork());
+			if (!pid)
+				ft_heredoc(io, fd, g_shell);
+			if (ft_exit_leaf(fd, &pid))
+				return ;
+			io->here_doc = fd[0];
+		}
+		else
+			io->expanded_value = ft_expand(io->value, g_shell);
+		io = io->next;
+	}
+}
+
 void	init_asttree(t_node *node, t_minishell *g_shell)
 {
 	if (!node)
 		return ;
+	if (node->type == N_PIPE || node->type == N_AND || node->type == N_OR)
+	{
+		init_asttree(node->left, g_shell);
+		if (!g_sig_handler.heredoc_sgint)
+			init_asttree(node->right, g_shell);
+	}
+	else
+		init_leaf(node, g_shell);
 }
