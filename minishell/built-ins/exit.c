082705b201773ffa64ae1bdc010bca1bb5e69ac3
surajbartoula <suraj_bartoula@yahoo.com>
2024-08-23 12:14:22 +0400

    exit should work

--- minishell/built-ins/exit.c
+++ minishell/built-ins/exit.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/21 11:35:05 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/08/23 12:13:03 by sbartoul         ###   ########.fr       */
+/*   Created: 2024/08/23 12:14:05 by sbartoul          #+#    #+#             */
+/*   Updated: 2024/08/23 12:14:07 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int	exit_with_code(char *args, t_minishell *g_shell)
 	if (!ft_isnum(args + i))
 	{
 		e_s = ft_err_msg((t_err){ENO_EXEC_255, ERRMSG_NUMERIC_REQUI, args});
-		(ft_clean(g_shell), exit(e_s));
+		(ft_clean(g_shell));
 	}
 	result = 0;
 	while (args[i])
@@ -75,15 +75,23 @@ int	ft_exit(t_minishell *g_shell, char **args)
 
 	if (args[1])
 	{
-		if (args[2])
+		if (!ft_isnum(args[1])) // Check if args[1] is not numeric
 		{
-			exit_status = ft_err_msg((t_err){ENO_GENERAL, ERRMSG_TOO_MANY_ARGS, NULL});
+			ft_putendl_fd("exit", STDOUT_FILENO);
+			exit_status = ft_err_msg((t_err){ENO_EXEC_255, ERRMSG_NUMERIC_REQUI, args[1]});
 			ft_clean(g_shell);
+			exit(exit_status);
 		}
-		else if (!ft_isnum(args[1]))
+		if (args[2]) // Check if there's a third argument
+		{
+			exit_status = ft_err_msg((t_err){ENO_GENERAL, ERRMSG_TOO_MANY_ARGS, NULL}); // Use ENO_GENERAL here
+			return (exit_status);
+		}
+		else // If args[1] is numeric and there is no third argument
 		{
 			exit_status = exit_with_code(args[1], g_shell);
-			(ft_clean(g_shell), exit(exit_status));
+			ft_clean(g_shell);
+			exit(exit_status);
 		}
 	}
 	else
@@ -91,4 +99,5 @@ int	ft_exit(t_minishell *g_shell, char **args)
 		ft_clean(g_shell);
 		exit(exit_status);
 	}
+	return (exit_status);
 }
