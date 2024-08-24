1398b1519119abf2352eeecba7920a4ff6ba591e
surajbartoula <suraj_bartoula@yahoo.com>
2024-08-24 12:19:55 +0400

    fixing norminette error

--- minishell/tokenization/appenders.c
+++ minishell/tokenization/appenders.c
@@ -6,7 +6,7 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 15:55:49 by oallan            #+#    #+#             */
-/*   Updated: 2024/08/23 15:41:42 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/08/24 12:19:08 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,26 @@ int	ft_append_separators(t_token_type t, char **line, t_token **token_list)
 	return (1);
 }
 
+int	ft_parse_identifier(char **line, size_t *i, t_minishell *g)
+{
+	char	*arr;
+
+	arr = *line;
+	while (arr[*i] && !ft_is_separator(arr + *i))
+	{
+		if (ft_is_spchar(arr[*i]))
+			return (ft_err_spchar(arr[*i]));
+		else if (ft_is_quote(arr[*i]))
+		{
+			if (!ft_skip_quotes(arr, i))
+				return (ft_print_quotes_err(arr[*i], g), 0);
+		}
+		else
+			(*i)++;
+	}
+	return (1);
+}
+
 int	ft_append_identifiers(char **line, t_token **token_list, t_minishell *g)
 {
 	char	*arr;
@@ -50,18 +70,8 @@ int	ft_append_identifiers(char **line, t_token **token_list, t_minishell *g)
 
 	arr = *line;
 	i = 0;
-	while (arr[i] && !ft_is_separator(arr + i))
-	{
-		if (ft_is_spchar(arr[i]))
-			return (ft_err_spchar(arr[i]));
-		if (ft_is_quote(arr[i]))
-		{
-			if (!ft_skip_quotes(arr, &i))
-				return (ft_print_quotes_err(arr[i], g), 0);
-		}
-		else
-			i++;
-	}
+	if (!ft_parse_identifier(line, &i, g))
+		return (0);
 	value = ft_substr(arr, 0, i);
 	if (!value)
 		return (0);
